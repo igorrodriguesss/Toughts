0bac04089ae820b98284e6882b8dc3150d6987a3
Igor Rodrigues <igorr2693@gmail.com>
2022-03-28 14:30:41 -0300

    Funciolidade de busca dos pensamentos

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,9 +1,33 @@
     const Tought = require('../models/Tought')
     const User = require('../models/User')
 
+    const { Op } = require('sequelize')
+
     module.exports = class ToughtController {
         static async showToughts(req, res) {
-            res.render('toughts/home')
+
+            let search = ''
+
+            if(req.query.search) {
+                search = req.query.search
+            }
+
+            const toughtsData = await Tought.findAll({
+                include: User,
+                where: {
+                    title: {[Op.like]: `%${search}%` },
+                }
+            })
+
+            const toughts = toughtsData.map((result) => result.get({ plain: true }))
+
+            let toughtsQty = toughts.length
+
+            if(toughtsQty === 0) {
+                toughtsQty = false
+            }
+
+            res.render('toughts/home', {toughts})
         }
 
         static async Dashboard(req, res) {
@@ -30,7 +54,6 @@
             if(toughts.length === 0) {
                 emptyToughts = true
             }
-
             res.render('toughts/dashboard', {toughts} )
         }
 
