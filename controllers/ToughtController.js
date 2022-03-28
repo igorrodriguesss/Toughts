ff113e40c1ecbc8c6a570bebaf068ee4d98eb6f1
Igor Rodrigues <igorr2693@gmail.com>
2022-03-28 13:21:34 -0300

    Funciolidade de edição de pensamento

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -73,5 +73,32 @@
                     console.log('Aconteceu um erro' + error)
                 }
             }
+
+            static async updateTought(req, res) {
+                const id = req.params.id
+
+                const tought = await Tought.findOne({where: { id: id }, raw: true })
+
+                res.render('toughts/edit', {tought})
         }
-    
+
+        static async updateToughtSave(req, res) {
+            const id = req.body.id
+
+            const tought = {
+                title: req.body.title,
+            }
+
+            try {
+                await Tought.update(tought, {where: { id: id }})
+
+                req.flash('message', 'Pensamento atualizado com sucesso!')
+                
+                req.session.save(() => {
+                    res.redirect('/toughts/dashboard')
+                })
+            } catch(error) {    
+                console.log('Aconteceu um erro' + error)
+            }
+        }
+    }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,8 @@ const chechAuth = require('../helpers/auth').chechAuth
 
 router.get('/add', checkAuth, ToughtController.createTought)
 router.post('/add', checkAuth, ToughtController.createToughtSave)
+router.get('/edit/:id', checkAuth, ToughtController.updateTought)
+router.post('/edit', checkAuth, ToughtController.updateToughtSave)
 router.post('/remove', checkAuth, ToughtController.removeTought)
 router.get('/dashboard', checkAuth, ToughtController.Dashboard)
 router.get('/', ToughtController.showToughts)
